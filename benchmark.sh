4d1741e0263c86fe4eb68aa799c028dcf9e4c805
Kath Nicholls <nichollskc@gmail.com>
2021-01-08 17:45:49 +0000

    Add previous commit as another baseline

--- benchmark.sh
+++ benchmark.sh
@@ -8,9 +8,19 @@ cd  "${TRAVIS_REPO_SLUG}-bench" && \
 
 # Bench master
 git checkout master && \
-cargo bench && \
+cargo bench -- --save-baseline master && \
+cargo run --example save_to_file resources/tests/large_100/ master_large_100.sst && \
+
+# Bench current branch
+git checkout ${TRAVIS_COMMIT}^1 && \
+cargo bench -- --save-baseline before && \
+cargo run --example save_to_file resources/tests/large_100/ before_large_100.sst && \
 
 # Bench current branch
 git checkout ${TRAVIS_COMMIT} && \
-cargo bench
+cargo bench -- --save-baseline current && \
+cargo run --example save_to_file resources/tests/large_100/ current_large_100.sst && \
+
+# Display the file sizes
+ls -lh ./
 
